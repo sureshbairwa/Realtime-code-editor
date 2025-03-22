6bb94d3d6726f79268021579a8728e2f3c179971
sureshbairwa <sureshbairwa@iitbhilai.ac.in>
2025-03-22 20:08:49 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: process.env.FRONTEND_URL,
         methods: ['GET', 'POST'],
         credentials: true
 
